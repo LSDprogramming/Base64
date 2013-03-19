11498a56844af1e51d68b631d1d46adc65caf2af
frumin <tomzaworowski@detroitlabs.com>
2013-03-19 13:53:34 -0400

    Initial Commit

---
+++ Base64/LSDViewController.m
@@ -0,0 +1,29 @@
+//
+//  LSDViewController.m
+//  Base64
+//
+//  Created by Tom Zaworowski on 3/19/13.
+//  Copyright (c) 2013 LSD programming LLC. All rights reserved.
+//
+
+#import "LSDViewController.h"
+
+@interface LSDViewController ()
+
+@end
+
+@implementation LSDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
